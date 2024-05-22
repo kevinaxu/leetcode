25353b663b8ac02f1adfb91054255c5e91b4ec5c
Kevin Xu <kevinxu16@gmail.com>
2024-05-22 08:56:20 -0400

    LL problem

---
+++ linked-list/287-find-duplicate-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        '''
+        Key Intuition:
+        - approach is very similar to detectCycle in Linked List 
+        - fast / slow pointers
+        - if values are the same, 
+        '''
+        pass
+
+
+nums = [1,3,4,2,2]
